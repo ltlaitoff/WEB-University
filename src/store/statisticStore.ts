c7b1dff8b05230156452fedba03ea3fcbff61771
Shchedrovskyi Ivan <ltlaitoff@gmail.com>
2023-11-24 13:11:25 +0200

    feat: Create statisticStore

---
+++ src/store/statisticStore.ts
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia'
+import { Statistic } from '../types/Statistic'
+
+interface StatisticStore {
+	statistic: Statistic[]
+}
+
+export const useStatistic = defineStore('statistic', {
+	state: (): StatisticStore => ({
+		statistic: []
+	}),
+
+	actions: {
+		add(statisticItem: Statistic) {
+			this.statistic.push(statisticItem)
+		},
+		delete(statisticItem: Statistic) {
+			this.statistic = this.statistic.filter(
+				item => item._id !== statisticItem._id
+			)
+		}
+	},
+
+	persist: true
+})
