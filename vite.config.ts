6924cf7195e163de9cbebbc391a366922738c46c
Shchedrovskyi Ivan <ltlaitoff@gmail.com>
2023-11-24 13:10:22 +0200

    chore: Create basic aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+	resolve: {
+		alias: {
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@store': path.resolve(__dirname, './src/store'),
+			'@types': path.resolve(__dirname, './src/types'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@': path.resolve(__dirname, './src')
+		}
+	},
 	plugins: [
 		vue(),
 		svgLoader({
