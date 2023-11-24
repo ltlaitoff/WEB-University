42af5f42a1b4cb94d68caecd980584f5f79cc597
Shchedrovskyi Ivan <ltlaitoff@gmail.com>
2023-11-24 13:10:47 +0200

    feat: Create categories store

---
+++ src/store/categoriesStore.ts
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia'
+import { Category } from '@types'
+
+interface CategoryStore {
+	categories: Category[]
+}
+
+export const useCategoryStore = defineStore('category', {
+	state: (): CategoryStore => ({
+		categories: [
+			{
+				_id: '0',
+				color: 'red',
+				mode: 'time',
+				name: 'Pomodoro'
+			}
+		]
+	}),
+
+	actions: {
+		add(categoriesItem: Category) {
+			this.categories.push(categoriesItem)
+		},
+
+		delete(categoriesItem: Category) {
+			this.categories = this.categories.filter(
+				item => item._id !== categoriesItem._id
+			)
+		}
+	},
+
+	persist: true
+})
