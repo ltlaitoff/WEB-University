420131f56b70db8f6439849030c9294ee45ba0cd
Shchedrovskyi Ivan <ltlaitoff@gmail.com>
2023-11-24 13:11:07 +0200

    feat: Create userSettingsStore

---
+++ src/store/userSettingsStore.ts
@@ -0,0 +1,53 @@
+import { defineStore } from 'pinia'
+import { Mode } from '../types/Mode'
+import { Colors } from '../types/Colors'
+import { Category } from '../types/Category'
+
+interface UserSettingsStore {
+	settings: {
+		colors: Record<Mode, Colors>
+		times: Record<Mode, number>
+		selectedMode: Mode
+		selectedCategory: Category
+	}
+}
+
+export const useUserSettingsStore = defineStore('settings', {
+	state(): UserSettingsStore {
+		return {
+			settings: {
+				colors: {
+					pomodoro: 'blue',
+					short: 'purple',
+					long: 'red'
+				},
+				times: {
+					pomodoro: 3 / 60,
+					short: 4 / 60,
+					long: 5 / 60
+				},
+				selectedMode: 'pomodoro',
+				selectedCategory: {
+					_id: '0',
+					color: 'red',
+					mode: 'time',
+					name: 'Pomodoro'
+				}
+			}
+		}
+	},
+	getters: {
+		colors: state => state.settings.colors,
+		times: state => state.settings.times,
+		selectedMode: state => state.settings.selectedMode
+	},
+	actions: {
+		setColor(mode: Mode, color: Colors) {
+			this.settings.colors[mode] = color
+		},
+		setSelectedMode(mode: Mode) {
+			this.settings.selectedMode = mode
+		}
+	},
+	persist: true
+})
