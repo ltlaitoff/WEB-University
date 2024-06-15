3d54e2612eb837fb3cd0027f2c87f48fa89cde21
Shchedrovskyi Ivan University <schedrovskiyivam@gmail.com>
2024-06-15 09:11:29 +0300

    feat(5): Connect i18n vue to project

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
@@ -10,10 +11,14 @@ import './style.css'
 
 const app = createApp(App)
 const pinia = createPinia()
+const i18n = createI18n({
+	// something vue-i18n options here ...
+})
 
 pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia)
+app.use(i18n)
 
 app.directive('close-modal', vCloseModal)
 
