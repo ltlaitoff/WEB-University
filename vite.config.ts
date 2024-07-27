e3aa5b97ca9cd87bb9590aac2cde3c19e89a2043
Shchedrovskyi Ivan <ltlaitoff@gmail.com>
2024-07-27 21:34:15 +0300

    init: Init base fsd structure

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,16 @@ import svgLoader from 'vite-svg-loader'
 export default defineConfig({
 	resolve: {
 		alias: {
+			'@app': path.resolve(__dirname, './src/app'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@widgets': path.resolve(__dirname, './src/widgets'),
+			'@features': path.resolve(__dirname, './src/features'),
+			'@entities': path.resolve(__dirname, './src/entities'),
+			'@shared': path.resolve(__dirname, './src/shared'),
+
 			'@assets': path.resolve(__dirname, './src/assets'),
 			'@components': path.resolve(__dirname, './src/components'),
 			'@constants': path.resolve(__dirname, './src/constants'),
-			'@pages': path.resolve(__dirname, './src/pages'),
 			'@store': path.resolve(__dirname, './src/store'),
 			'@types': path.resolve(__dirname, './src/types'),
 			'@utils': path.resolve(__dirname, './src/utils'),
